df119a63c7e250abdb636186977ac8352270f987
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-09 17:56:14 +0200

    Arreglado problema de ventanas de los productos

--- SCRIPTS/productos.js
+++ SCRIPTS/productos.js
@@ -1,3 +1,31 @@
+function mostrarVentana(titulo, infoAdicional, imagen) {
+    document.getElementById('tituloMenu').innerText = titulo;
+    document.getElementById('informacionAdicional').innerText = infoAdicional;
+    document.getElementById('imagenMenu').src = imagen;
+    document.getElementById('ventanaEmergente').style.display = 'block';
+}
+
+function cerrarVentana() {
+    document.getElementById('ventanaEmergente').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menus = document.querySelectorAll('.menu-1 img');
+    const imagenesMenu = [
+        '../IMG/Menu1.PNG', '../IMG/Menu1.PNG', '../IMG/Menu1.PNG',
+        '../IMG/meatbacon.jpg', '../IMG/meatbacon.jpg', '../IMG/meatbacon.jpg',
+        '../IMG/meatbacon.jpg', '../IMG/meatbacon.jpg' // Ajusta esta lista con las rutas correctas de las imágenes
+    ];
+    menus.forEach(function(menu, index) {
+        menu.addEventListener('click', function() {
+            const titulo = this.parentElement.querySelector('p').innerText;
+            const infoAdicional = ''; // Ajusta esto con la información adicional correspondiente
+            const imagen = imagenesMenu[index];
+            mostrarVentana(titulo, infoAdicional, imagen);
+        });
+    });
+});
+
 // Define un objeto para almacenar los productos en el carrito junto con su cantidad y su imagen
 let carrito = {};
 
