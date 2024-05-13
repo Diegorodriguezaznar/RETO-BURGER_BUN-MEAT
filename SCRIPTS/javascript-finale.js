8a477f4c41dcfbb670232da0582a75654b937cc2
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-13 21:41:45 +0200

    Añadir carrito y concepto visual de Contactanos

---
+++ SCRIPTS/javascript-finale.js
@@ -0,0 +1,42 @@
+
+window.onload = function () {
+    // Variables
+    const IMAGENES = [
+        '/IMG/mid-pollo.jpg',
+        '/IMG/Menu-gen.png',
+        '/IMG/TartaQueso.jpg'
+    ];
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 3500;
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen');
+    //let intervalo;
+
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    renderizarImagen();
+    setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+}
