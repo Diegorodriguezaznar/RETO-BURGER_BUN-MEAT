003262d90e076fab534d6b5ff974708d4244afb1
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-16 20:21:43 +0200

    boton útil en pagina finale

---
+++ SCRIPTS/base.js
@@ -0,0 +1,22 @@
+let placeYourOrderButton
+let placeYourOrderDiv
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM content loaded")
+    placeYourOrderButton = document.getElementsByClassName("bloques")[1]
+    placeYourOrderDiv = document.getElementsByClassName("place-order-options")[0]
+
+    placeYourOrderButton.addEventListener("mouseover", () => {
+        placeYourOrderDiv.style.display = "flex"
+    })
+    placeYourOrderButton.addEventListener("mouseout", () => {
+        placeYourOrderDiv.style.display = "none"
+    })
+
+    placeYourOrderDiv.addEventListener("mouseover", () => {
+        placeYourOrderDiv.style.display = "flex"
+    })
+    placeYourOrderDiv.addEventListener("mouseout", () => {
+        placeYourOrderDiv.style.display = "none"
+    })
+})
