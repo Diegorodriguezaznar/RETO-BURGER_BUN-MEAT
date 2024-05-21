775bc70e3ce38a147cd93121fcf73a83dbc8977a
Daniel <a25586@svalero.com>
2024-05-21 21:54:28 +0200

    Inttroducidos inserts de la base de datos

---
+++ SQL/iEmpleados.sql
@@ -0,0 +1,200 @@
+
+BEGIN
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-01', 'John', 'Michels', 'johnmichels@bunmeat.com', '212-555-9489', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-02', 'Jessica', 'Miller', 'jessicamiller@bunmeat.com', '646-555-7891', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-03', 'Michael', 'Brown', 'michaelbrown@bunmeat.com', '718-555-2345', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-04', 'Emily', 'Daily', 'emilydaily@bunmeat.com', '917-555-8765', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-05', 'Daniel', 'Johnson', 'danieljohnson@bunmeat.com', '917-555-3456', '1234', 'CE-2');
+COMMIT;
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-06', 'Olivia', 'Wilson', 'oliviawilson@bunmeat.com', '646-555-1234', '1234', 'CE-2');
+COMMIT;
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-07', 'William', 'Gaggings', 'williamgaggings@bunmeat.com', '718-555-6789', '1234', 'CE-3');
+COMMIT;
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-08', 'Sophia', 'Anderson', 'sophiaanderson@bunmeat.com', '212-555-5678', '1234', 'CE-3');
+COMMIT;
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-09', 'Ethan', 'Winters', 'ethanwinters@bunmeat.com', '917-555-4321', '1234', 'CE-4');
+COMMIT;
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-10', 'Ava', 'Nightless', 'avanightless@bunmeat.com', '718-555-9012', '1234', 'CE-2');
+COMMIT;
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-11', 'John', 'Smith', 'johnsmith@bunmeat.com', '718-555-1001', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-12', 'Emily', 'Johnson', 'emilyjohnson@bunmeat.com', '718-555-1002', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-13', 'Michael', 'Williams', 'michaelwilliams@bunmeat.com', '718-555-1003', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-14', 'Emma', 'Brown', 'emmabrown@bunmeat.com', '718-555-1004', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-15', 'Olivia', 'Jones', 'oliviajones@bunmeat.com', '718-555-1005', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-16', 'William', 'Garcia', 'williamgarcia@bunmeat.com', '718-555-1006', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-17', 'Sophia', 'Miller', 'sophiamiller@bunmeat.com', '718-555-1007', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-18', 'James', 'Martinez', 'jamesmartinez@bunmeat.com', '718-555-1008', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-19', 'Mia', 'Davis', 'miadavis@bunmeat.com', '718-555-1009', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-20', 'Benjamin', 'Rodriguez', 'benjaminrodriguez@bunmeat.com', '718-555-1010', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-21', 'Ava', 'Martinez', 'avamartinez@bunmeat.com', '718-555-1011', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-22', 'Isabella', 'Hernandez', 'isabellahernandez@bunmeat.com', '718-555-1012', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-23', 'Elijah', 'Lopez', 'elijahlopez@bunmeat.com', '718-555-1013', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-24', 'Charlotte', 'Gonzalez', 'charlottegonzalez@bunmeat.com', '718-555-1014', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-25', 'Lucas', 'Wilson', 'lucaswilson@bunmeat.com', '718-555-1015', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-26', 'Amelia', 'Anderson', 'ameliaanderson@bunmeat.com', '718-555-1016', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-27', 'Alexander', 'Thomas', 'alexanderthomas@bunmeat.com', '718-555-1017', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-28', 'Mason', 'Taylor', 'masontaylor@bunmeat.com', '718-555-1018', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-29', 'Evelyn', 'Moore', 'evelynmoore@bunmeat.com', '718-555-1019', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-30', 'Henry', 'Jackson', 'henryjackson@bunmeat.com', '718-555-1020', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-31', 'Harper', 'Martin', 'harpermartin@bunmeat.com', '718-555-1021', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-32', 'Daniel', 'Lee', 'daniellee@bunmeat.com', '718-555-1022', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-33', 'Liam', 'Perez', 'liamperez@bunmeat.com', '718-555-1023', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-34', 'Aria', 'Thompson', 'ariathompson@bunmeat.com', '718-555-1024', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-35', 'Sebastian', 'White', 'sebastianwhite@bunmeat.com', '718-555-1025', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-36', 'Sofia', 'Harris', 'sofiaharris@bunmeat.com', '718-555-1026', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-37', 'David', 'Sanchez', 'davidsanchez@bunmeat.com', '718-555-1027', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-38', 'Ella', 'Clark', 'ellaclark@bunmeat.com', '718-555-1028', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-39', 'Matthew', 'Ramirez', 'matthewr@bunmeat.com', '718-555-1029', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-40', 'Scarlett', 'Lewis', 'scarlettlewis@bunmeat.com', '718-555-1030', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-41', 'Jack', 'Robinson', 'jackrobinson@bunmeat.com', '718-555-1031', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-42', 'Victoria', 'Walker', 'victoriawalker@bunmeat.com', '718-555-1032', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-43', 'Jackson', 'Young', 'jacksonyoung@bunmeat.com', '718-555-1033', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-44', 'Lily', 'Allen', 'lilyallen@bunmeat.com', '718-555-1034', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-45', 'Joseph', 'King', 'josephking@bunmeat.com', '718-555-1035', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-46', 'Zoey', 'Scott', 'zoeyscott@bunmeat.com', '718-555-1036', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-47', 'Gabriel', 'Green', 'gabrielgreen@bunmeat.com', '718-555-1037', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-48', 'Penelope', 'Baker', 'penelopebaker@bunmeat.com', '718-555-1038', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-49', 'Aiden', 'Adams', 'aidenadams@bunmeat.com', '718-555-1039', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-50', 'Riley', 'Rivera', 'rileyrivera@bunmeat.com', '718-555-1040', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-51', 'Carter', 'Nelson', 'carternelson@bunmeat.com', '718-555-1041', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-52', 'Grace', 'Carter', 'gracecarter@bunmeat.com', '718-555-1042', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-53', 'Isaac', 'Mitchell', 'isaacmitchell@bunmeat.com', '718-555-1043', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-54', 'Elizabeth', 'Perez', 'elizabethperez@bunmeat.com', '718-555-1044', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-55', 'Julian', 'Roberts', 'julianroberts@bunmeat.com', '718-555-1045', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-56', 'Hannah', 'Turner', 'hannahturner@bunmeat.com', '718-555-1046', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-57', 'Levi', 'Phillips', 'leviphillips@bunmeat.com', '718-555-1047', '1234', 'CE-2');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-58', 'Zoey', 'Campbell', 'zoeycampbell@bunmeat.com', '718-555-1048', '1234', 'CE-3');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-59', 'Isaiah', 'Parker', 'isaiahparker@bunmeat.com', '718-555-1049', '1234', 'CE-1');
+
+INSERT INTO EMPLEADOS
+VALUES ('EM-60', 'Lily', 'Evans', 'lilyevans@bunmeat.com', '718-555-1050', '1234', 'CE-2');
+
+
+
+EXCEPTION
+
+WHEN OTHERS THEN
+
+
+ROLLBACK;
+
+END;
+
---
+++ SQL/iProductos.sql
@@ -0,0 +1,108 @@
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-01', 'Meat_Veal', 'Beef Veal burger with cheese', 8.99, 'Beef Veal burger with cheese, lettuce and sauces (few chips include).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-02', 'Meat_Veal_Complete', 'Beef Veal burger with double cheese', 10.99, 'Beef Veal burger with double cheese, lettuce, sauces and bacon (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-03', 'Meat_Veal_SMASH', 'Double Beef Veal burger with double cheese', 13.99, 'Double Beef Veal burger with double cheese, lettuce, sauces, bacon and onion (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-04', 'Meat_Beef', 'Beef burger with cheese', 9.99, 'Beef burger with cheese, lettuce and sauces (few chips include).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-05', 'Meat_Beef_Complete', 'Beef burger with double cheese', 12.99, 'Beef burger with double cheese, lettuce, sauces and bacon (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-06', 'Meat_Beef_SMASH', 'Double Beef burger with double cheese', 14.99, 'Double Beef burger with double cheese, lettuce, sauces, bacon and onion (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-07', 'Meat_Chicken', 'Chicken burger with cheese', 7.99, 'Chicken burger with cheese, lettuce and sauces (few chips include).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-08', 'Meat_Chicken_Complete', 'Chicken burger with double cheese', 9.99, 'Chicken burger with double cheese, lettuce, sauces and bacon (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-09', 'Meat_Chicken_SMASH', 'Double chicken burger with double cheese', 11.99, 'Double chicken burger with double cheese, lettuce, sauces, bacon and onion (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-10', 'Meat_Ternasco', 'Ternasco burger with cheese', 9.99, 'Ternasco burger with cheese, lettuce and sauces (few chips include).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-11', 'Meat_Ternasco_Complete', 'Ternasco burger with double cheese', 10.99, 'Ternasco burger with double cheese, lettuce, sauces and bacon (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-12', 'Meat_Ternasco_SMASH', 'Double Ternasco burger with double cheese', 13.99, 'Double Ternasco burger with double cheese, lettuce, sauces, bacon and onion (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-13', 'Meat_Vegan', 'Vegan burger with tofu', 10.99, 'Vegan burger with tofu, lettuce and pickles (few chips include).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-14', 'Meat_Vegan_Complete', 'Vegan burger with double tofu', 12.99, 'Vegan burger with double tofu, lettuce, double pickles and onion (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-15', 'Meat_Vegan_SMASH', 'Double Vegan burger with double tofu', 15.99, 'Double Vegan burger with double tofu, lettuce, pickles, double onion and chips inside (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-16', 'Meat_Kraken', 'Squids burger with cheese', 8.99, 'Squids burger with cheese, lettuce and sauces (few chips include).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-17', 'Meat_Kraken_Complete', 'Squids burger with double cheese', 9.99, 'Squids burger with double cheese, lettuce, sauces and bacon (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-18', 'Meat_Kraken_SMASH', 'Double Squid burger with double cheese', 11.99, 'Double Squid burger with double cheese, lettuce, sauces, bacon and onion (includes some fries).', 'CP-1');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-19', 'Nachos', 'Nachos with guacamole sauce', 5.99, 'Nachos with guacamole sauce.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-20', 'Onion_Rings', 'Onion rings with BBQ sauce and yogurt sauce', 6.99, 'Onion rings with BBQ sauce and yogurt sauce.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-21', 'French_fries', 'Those of a lifetime', 4.99, 'Those of a lifetime.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-22', 'Meat_balls','Meatballs with tomato', 7.99, 'Meatballs with tomato.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-23', 'Chicken_fingers', '8 Chicken fingers', 6.99, '8 Chicken fingers.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-24', 'Fries_deluxe', 'Deluxe potatoes with deluxe sauce', 5.99, 'Deluxe potatoes with deluxe sauce.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-25', 'Croquettes', '4 Croquettes, 2 ham and cheese / 2 meat', 5.99, '4 Croquettes, 2 ham and cheese / 2 meat.', 'CP-2');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-26', 'CheeseCake', 'Homemade cheesecake', 4.99, 'Homemade cheesecake.', 'CP-3');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-27', 'CarrotCake', 'Homemade Carrot cake', 3.99, 'Homemade Carrot cake.', 'CP-3');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-28', 'ChocolateCake', 'Homemade Chocolate cake', 3.99, 'Homemade Chocolate cake.', 'CP-3');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-29', 'StrawberryCake', 'Homemade Strawberry cake', 3.99, 'Homemade Strawberry cake.', 'CP-3');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-30', 'Choclate_mousse', 'Chocolate Mousse with forest fruits', 3.99, 'Chocolate Mousse with forest fruits.', 'CP-3');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-31', 'Custard', 'Flan with cream', 3.99, 'Flan with cream.', 'CP-3');
+
+INSERT INTO PRODUCTOS (id_producto, imagen, nombre, precio, descripcion, id_categoriaProducto)
+VALUES ('P-32', 'Churros', 'Churros with Chocolate Cup', 4.99, 'Churros with Chocolate Cup.', 'CP-3');
+
+COMMIT;
+
+EXCEPTION
+
+WHEN OTHERS THEN
+
+DBMS_OUTPUT.PUT_LINE('ERROR EN LA TRANSACCION:'||SQLERRM); --pueden ser otro tipo de mensaje
+
+
+ROLLBACK;
+
+END;
